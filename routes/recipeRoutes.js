cb02755a5a64dc0e1a58905890e5d9e977680edf
Elvis Knapman <elvis@elvisknapman.com>
2019-09-12 18:20:05 -0400

    :sparkles: wrote routes
    wrote routes and handlers for getting all recipes, getting all ingredients by recipe and getting instructions for a recipe

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,7 +1,48 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "main route" });
+// recipes model
+const Recipes = require("../recipes/recipe-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Recipes.getRecipes();
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server encountered error retrieving recipes" });
+  }
+});
+
+router.get("/:id/ingredients", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await Recipes.getIngredientList(id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server encountered error retrieving ingredients for recipe"
+    });
+  }
+});
+
+router.get("/:id/instructions", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await Recipes.getInstructions(id);
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: "Invalid ID" });
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Server encountered error retrieving instructions" });
+  }
 });
 
 module.exports = router;
