e4b2ca52a81f43e800bcb47b14e41e7ba7468679
Elvis Knapman <elvis@elvisknapman.com>
2019-09-12 18:43:58 -0400

    :sparkles: added route to get all ingredients

---
+++ routes/ingredientsRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+// models
+const Ingredients = require("../ingredients/ingredients-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Ingredients.getAllIngredients();
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server encountered error retrieving list of ingredients"
+    });
+  }
+});
+
+router.get("/:id/recipes", async (req, res) => {});
+
+module.exports = router;
