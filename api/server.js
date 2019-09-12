964d6a552c3ec07328c84c886d38e7aa1f10dbbe
Elvis Knapman <elvis@elvisknapman.com>
2019-09-12 18:44:32 -0400

    :sparkles: imported ingredient routes anddefined route

--- api/server.js
+++ api/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const server = express();
 
 const recipeRoutes = require("../routes/recipeRoutes.js");
+const ingredientRoutes = require("../routes/ingredientsRoutes.js");
 
 server.use(express.json());
 
 server.use("/api/recipes", recipeRoutes);
+server.use("/api/ingredients", ingredientRoutes);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API up and running..." });
